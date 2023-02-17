222a1aeff2a31011750cb88d864b98305a497478
Emerson Batista <84859510+emersongg21@users.noreply.github.com>
2023-02-17 14:01:06 +0000

    Added "Scroll to top" button

--- PageTricks/SmoothScrool/script.js
+++ PageTricks/SmoothScrool/script.js
@@ -11,3 +11,16 @@ document.addEventListener("visibilitychange", function() {
     document.title = originalTitle;
   }
 });
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scroll-to-top").style.display = "block";
+  } else {
+    document.getElementById("scroll-to-top").style.display = "none";
+  }
+};
+
+document.getElementById("scroll-to-top").addEventListener("click", function() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
