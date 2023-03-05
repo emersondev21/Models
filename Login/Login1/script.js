50c4e2cf1550cee0db70a00edce043a2672a302c
Emerson Batista <84859510+emersongg21@users.noreply.github.com>
2023-03-05 17:26:35 +0000

    Update "Login1"

---
+++ Login/Login1/script.js
@@ -0,0 +1,8 @@
+function capturar(){
+  let email = document.getElementById("email").value;
+  console.log("email: " + email);
+
+  let senha = document.getElementById("senha").value;
+  console.log("senha: " + senha);
+
+}
